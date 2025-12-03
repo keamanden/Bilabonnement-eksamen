7b7d1f2ffec366f4aa1464b440b844c951f78416
Keamanden <than0002@stud.kea.dk>
2025-12-03 15:54:17 +0100

    startet op på login.html og login_style.css

--- src/main/java/com/example/demo/service/UserService.java
+++ src/main/java/com/example/demo/service/UserService.java
@@ -8,8 +8,12 @@ import org.springframework.stereotype.Service;
 @Service
 public class UserService {
 
-    @Autowired
-    private UserRepository userRepository;
+
+    private final UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
 
     public boolean checkLogin(String username, String password) {
 
